023c9b0aea401b5ef371b8df8c1efbb1c6053536
Raphaël M. <Galixte@users.noreply.github.com>
2015-05-20 07:13:13 +0200

    Update of FRENCH translation for Posts Merging extension
    Update for info_acp_posts_merging.php file.

--- language/fr/info_acp_posts_merging.php
+++ language/fr/info_acp_posts_merging.php
@@ -2,16 +2,15 @@
 /**
 *
 * Posts Merging extension for the phpBB Forum Software package.
-*
-* @copyright (c) 2013 phpBB Limited <https://www.phpbb.com>
-* @license GNU General Public License, version 2 (GPL-2.0)
-*
 * French translation by Galixte (http://www.galixte.com)
+*
+* @copyright (c) 2015 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
 
 /**
-* @ignore
+* DO NOT CHANGE
 */
 if (!defined('IN_PHPBB'))
 {
@@ -34,6 +33,10 @@ if (empty($lang) || !is_array($lang))
 // You do not need this where single placeholders are used, e.g. 'Message %d' is fine
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
 
 $lang = array_merge($lang, array(
 	'ACP_POSTS_MERGING'				=> 'Fusion des messages',
@@ -43,7 +46,7 @@ $lang = array_merge($lang, array(
 	'MERGE_NO_TOPICS'				=> 'Sujets exclus',
 	'MERGE_NO_TOPICS_EXPLAIN'		=> 'Si cette fonctionnalité est activée, indiquez les IDs des sujets pour lesquels vous ne souhaitez par activer cette fonctionnalité (séparés par une virgule).',
 	'MERGE_NO_FORUMS'				=> 'Forums exclus',
-	'MERGE_NO_FORUMS_EXPLAIN'		=> 'Si cette fonctionnalité est activée, indiquez les IDs des forums contenant les sujets pour lesquels vous ne souhaitez par activer cette fonctionnalité (séparés par une virgule).',
+	'MERGE_NO_FORUMS_EXPLAIN'		=> 'La fonctionnalité de fusion des messages <strong>sera désactivé dans les forums sélectionnés</strong>. Sélectionner aucun forum pour utiliser la fonctionnalité de fusion des messages dans tous les forums.<br />Pour sélectionner / désélectionner plusieurs forums utiliser la combinaison de la touche <samp>CTRL</samp> tout en cliquant.',
 	'MERGE_SEPARATOR'				=> 'Separator',
 	'MERGE_SEPARATOR_EXPLAIN'		=> 'Here you can configure the separator which will appear between the merged message parts.<br />You can use BBCodes which will be parsed in according to the board or message settings.<br /><br />You can also use any language string present in your language/ directory like this: {L_<em>&lt;STRINGNAME&gt;</em>} where <em>&lt;STRINGNAME&gt;</em> is the name of the translated string you want to add. For example, {L_WROTE} will be displayed as “wrote” or its translation according to user’s locale.<br /><br />Use <em>&#37;s</em> placeholder (once) to include the time passed between merging in the separator.',
 ));
